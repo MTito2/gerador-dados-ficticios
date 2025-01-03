690c2f3c35f490f5a68858f889abd750388f4abf
Mateus da Cunha Tito <134788822+MTito2@users.noreply.github.com>
2025-01-03 07:26:59 -0300

    Adiciona cargos

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import gender_guesser.detector as gender
 from faker import Faker
+import random
 
 fake = Faker("pt-PT")
 
@@ -20,3 +21,27 @@ def descobrir_genero(nome_completo):
     detector_genero = gender.Detector()
     genero = "M" if detector_genero.get_gender(nome) in ("male", "mostly_male", "andy") else "F"
     return genero
+
+def definir_cargos():
+    cargos = [
+        "CEO",
+        "Desenvolvedor de Software Júnior",
+        "Desenvolvedor de Software Pleno",
+        "Desenvolvedor de Software Sênior",
+        "Engenheiro de DevOps",
+        "Product Manager",
+        "Designer de UX/UI",
+        "Especialista em Marketing Digital",
+        "Analista de dados",
+        "Especialista em Segurança da Informação",
+        "Gestor de Vendas",
+        "Auxiliar de serviços gerais",
+        "Assitente de TI (Suporte Técnico)",
+        "Gerente de TI",
+        "Coordenador de Atendimento ao Cliente"
+    ]
+
+    peso = [1, 40, 20, 2, 2, 10, 2, 2, 20, 2, 40, 35, 2, 15, 2, 10, 20, 30, 2, 2]
+    cargo = random.choices(cargos, weights=peso, k=1)
+
+    return cargo
